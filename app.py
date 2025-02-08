091cf0851b8b70632cbb1c61c9fad89ab930dd08
rheaacharya77 <ria.acharya07@gmail.com>
2025-02-08 14:42:59 +0545

    code refactor

--- app.py
+++ app.py
@@ -35,3 +35,126 @@ Format your response as follows:
 
 Important: Base your entire response solely on the information provided in the context. Do not include any external knowledge or assumptions not present in the given text.
 """
+
+def process_document(uploaded_file):
+    # Store uploaded file as a temp file
+    temp_file = tempfile.NamedTemporaryFile("wb", suffix=".pdf", delete=False)
+    temp_file.write(uploaded_file.read())
+
+    loader = PyMuPDFLoader(temp_file.name)
+    docs = loader.load()
+    os.unlink(temp_file.name)  # Delete temp file
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400,
+        chunk_overlap=100,
+        separators=["\n\n", "\n", ".", "?", "!", " ", ""],
+    )
+
+    return text_splitter.split_documents(docs)
+
+def get_vector_collection():
+    ollama_ef = OllamaEmbeddingFunction(
+        url="http://localhost:11434/api/embeddings",
+        model_name="nomic-embed-text:latest",
+    )
+
+    chroma_client = chromadb.PersistentClient(path="./demo-rag-chroma")
+    return chroma_client.get_or_create_collection(
+        name="rag_app",
+        embedding_function=ollama_ef,
+        metadata={"hnsw:space": "cosine"},
+    )
+
+def add_to_vector_collection(all_splits,file_name):
+    collection = get_vector_collection()
+    documents, metadatas, ids = [], [], []
+
+    for idx, split in enumerate(all_splits):
+        documents.append(split.page_content)
+        metadatas.append(split.metadata)
+        ids.append(f"{file_name}_{idx}")
+
+    collection.upsert(
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
+    )
+    st.success("Data added to the vector store!")
+
+def query_collection(prompt, n_results = 10):
+    collection = get_vector_collection()
+    results = collection.query(query_texts=[prompt], n_results=n_results)
+    return results
+
+def call_llm(context, prompt):
+    response = ollama.chat(
+        model="llama3.2",
+        stream=True,
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt,
+            },
+            {
+                "role": "user",
+                "content": f"Context: {context}, Question: {prompt}",
+            },
+        ],
+    )
+    for chunk in response:
+        if chunk["done"] is False:
+            yield chunk["message"]["content"]
+        else:
+            break
+
+def re_rank_cross_encoders(documents):
+    relevant_text = ""
+    relevant_text_ids = []
+
+    encoder_model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+    ranks = encoder_model.rank(prompt, documents, top_k=3)
+    for rank in ranks:
+        relevant_text += documents[rank["corpus_id"]]
+        relevant_text_ids.append(rank["corpus_id"])
+
+    return relevant_text, relevant_text_ids
+
+# run the app
+if __name__ == '__main__':
+    # Document Upload Area
+    with st.sidebar:
+        st.set_page_config(page_title="RAG Question Answer")
+        uploaded_file = st.file_uploader(
+            "**📑 Upload PDF files**", type=["pdf"], accept_multiple_files=False
+        )
+
+        process = st.button(
+            "Process",
+        )
+        if uploaded_file and process:
+            normalize_uploaded_file_name = uploaded_file.name.translate(
+                str.maketrans({"-": "_", ".": "_", " ": "_"})
+            )
+            # all_splits = process_document(uploaded_file)
+            # add_to_vector_collection(all_splits, normalize_uploaded_file_name)
+
+    # Question Answer Area
+    st.header("RAG Question Answer")
+    prompt = st.text_area("**Ask a question related to your document:**")
+    ask = st.button(
+        "Submit",
+    )
+    # if ask and prompt:
+    #     results = query_collection(prompt)
+    #     context = results.get("documents")[0]
+    #     relevant_text, relevant_text_ids = re_rank_cross_encoders(context)
+    #     response = call_llm(context=relevant_text, prompt=prompt)
+    #     st.write_stream(response)
+
+    #     with st.expander("See retrieved documents"):
+    #         st.write(results)
+
+    #     with st.expander("See most relevant document ids"):
+    #         st.write(relevant_text_ids)
+    #         st.write(relevant_text)
